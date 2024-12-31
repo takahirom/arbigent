83c12edb7ca0c3235377d97dd5b73f69a4dce38a
takahirom <takam.dev@gmail.com>
2024-12-31 21:47:34 +0900

    Add a test

--- sample-test/src/test/kotlin/ArbiterScenarioExecutorTest.kt
+++ sample-test/src/test/kotlin/ArbiterScenarioExecutorTest.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import maestro.orchestra.MaestroCommand
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ArbiterScenarioExecutorTest {
   @OptIn(ExperimentalStdlibApi::class)
@@ -26,42 +27,51 @@ class ArbiterScenarioExecutorTest {
           }
         }
       )
+    }
+    val arbiterScenarioExecutor = ArbiterScenarioExecutor {
+    }
+    val arbiterExecutorScenario = ArbiterScenarioExecutor.ArbiterExecutorScenario(
+      listOf(
+        ArbiterScenarioExecutor.ArbiterAgentTask("goal1", agentConfig),
+        ArbiterScenarioExecutor.ArbiterAgentTask("goal2", agentConfig)
+      ),
+      maxStepCount = 10,
+    )
+    arbiterScenarioExecutor.executeAsync(
+      arbiterExecutorScenario
+    )
+    advanceUntilIdle()
+  }
+
+  @OptIn(ExperimentalStdlibApi::class)
+  @Test
+  fun lastInterceptorCalledFirst() = runTest {
+    ArbiterCorotuinesDispatcher.dispatcher = coroutineContext[CoroutineDispatcher]!!
+    val order = mutableListOf<Int>()
+    val agentConfig = AgentConfig {
+      device(FakeDevice())
+      ai(FakeAi())
       addInterceptor(
-        object : ArbiterStepInterceptor {
+        object : ArbiterInitializerInterceptor {
           override fun intercept(
-            stepInput: ArbiterAgent.StepInput,
-            chain: ArbiterStepInterceptor.Chain
-          ): ArbiterAgent.StepResult {
-            println("Arbiter ArbiterStepInterceptor test intercept stepInput start")
-            val result = chain.proceed(stepInput)
-            println("Arbiter ArbiterStepInterceptor test intercept stepInput end")
-            return result
+            device: ArbiterDevice,
+            chain: ArbiterInitializerInterceptor.Chain
+          ) {
+            order.add(200)
+            chain.proceed(device)
+            order.add(300)
           }
         }
       )
       addInterceptor(
-        object : ArbiterDecisionInterceptor {
+        object : ArbiterInitializerInterceptor {
           override fun intercept(
-            decisionInput: ArbiterAi.DecisionInput,
-            chain: ArbiterDecisionInterceptor.Chain
-          ): ArbiterAi.DecisionOutput {
-            println("  intercept decisionInput: $decisionInput")
-            val result = chain.proceed(decisionInput)
-            println("  intercept decisionInput result: $result")
-            return result
-          }
-        }
-      )
-      addInterceptor(
-        object : ArbiterExecuteCommandsInterceptor {
-          override fun intercept(
-            executeCommandsInput: ArbiterAgent.ExecuteCommandsInput,
-            chain: ArbiterExecuteCommandsInterceptor.Chain
-          ): ArbiterAgent.ExecuteCommandsOutput {
-            println("    intercept executeCommandsInput: $executeCommandsInput")
-            val result = chain.proceed(executeCommandsInput)
-            println("    intercept executeCommandsInput result: $result")
-            return result
+            device: ArbiterDevice,
+            chain: ArbiterInitializerInterceptor.Chain
+          ) {
+            order.add(100)
+            chain.proceed(device)
+            order.add(400)
           }
         }
       )
@@ -79,5 +89,6 @@ class ArbiterScenarioExecutorTest {
       arbiterExecutorScenario
     )
     advanceUntilIdle()
+    assertEquals(listOf(100, 200, 300, 400, 100, 200, 300, 400), order)
   }
 }
