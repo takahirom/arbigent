c629e14e65dc83d99a6f89fcf23f5218d09fa57f
takahirom <takam.dev@gmail.com>
2025-01-14 11:07:54 +0900

    Fix auto focus bug

--- arbigent-core/src/main/java/io/github/takahirom/arbigent/ArbigentPrompts.kt
+++ arbigent-core/src/main/java/io/github/takahirom/arbigent/ArbigentPrompts.kt
@@ -1,7 +1,7 @@
 package io.github.takahirom.arbigent
 
 public object ArbigentPrompts {
-  public val systemPrompt: String = "Take a deep breath. You are an agent that achieves the user's goal automatically. Please don't do anything the user doesn't want to do. Please be careful not to repeat the same action.\n"
+  public val systemPrompt: String = "Take a deep breath. You are an agent that achieves the user's goal automatically. Please don't do anything the user doesn't want to do. Please be careful not to repeat the same action. It's better to achieve users' goals with the fewest number of actions.\n"
   public val systemPromptForTv: String =
     "$systemPrompt Tips for D-pad navigation: If you know where to move, please use DpadAutoFocus. If you can't move the focus with the D-pad, try pressing the D-pad twice or different direction. For selecting an item, use the center button."
   public val imageAssertionSystemPrompt: String = """Evaluate the following assertion for fulfillment in the new image.
