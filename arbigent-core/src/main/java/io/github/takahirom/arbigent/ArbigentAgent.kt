4cc069189f39b0c4a5dfc9d69a18b12d99519882
takahirom <takam.dev@gmail.com>
2025-01-27 11:26:17 +0900

    Extract detectStuckScreen

--- arbigent-core/src/main/java/io/github/takahirom/arbigent/ArbigentAgent.kt
+++ arbigent-core/src/main/java/io/github/takahirom/arbigent/ArbigentAgent.kt
@@ -707,31 +707,3 @@ private fun step(
   )
   return StepResult.Continue
 }
-
-private fun detectStuckScreen(lastScreenshot: String?, newScreenshot: File): Boolean {
-  if (lastScreenshot == null) {
-    return false
-  }
-  val lastScreenshotFile = File(lastScreenshot)
-  if (lastScreenshotFile.exists().not()) {
-    return false
-  }
-  val oldBufferedImage = ImageIO.read(lastScreenshotFile)
-  val newBufferedImage = ImageIO.read(newScreenshot)
-  try {
-    if (oldBufferedImage.width != newBufferedImage.width || oldBufferedImage.height != newBufferedImage.height) {
-      return false
-    }
-    for (x in 0 until oldBufferedImage.width) {
-      for (y in 0 until oldBufferedImage.height) {
-        if (oldBufferedImage.getRGB(x, y) != newBufferedImage.getRGB(x, y)) {
-          return false
-        }
-      }
-    }
-    return true
-  } finally {
-    oldBufferedImage.flush()
-    newBufferedImage.flush()
-  }
-}
